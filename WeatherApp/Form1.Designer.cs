81629039bf3fc598e6c4706784b22bf9eee22086
Hafeez84 <H.qasemy@live.com>
2019-06-14 11:26:37 +0300

    Initial with some changes...

--- WeatherApp/Form1.Designer.cs
+++ WeatherApp/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxCities = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 322);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // comboBoxCities
+            // 
+            this.comboBoxCities.FormattingEnabled = true;
+            this.comboBoxCities.Location = new System.Drawing.Point(196, 79);
+            this.comboBoxCities.Name = "comboBoxCities";
+            this.comboBoxCities.Size = new System.Drawing.Size(451, 28);
+            this.comboBoxCities.TabIndex = 1;
+            this.comboBoxCities.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxCities);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxCities;
     }
 }
 
--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -12,9 +12,41 @@ namespace WeatherApp
 {
     public partial class Form1 : Form
     {
+        private Dictionary<String, String> citis = new Dictionary<string, string>();
         public Form1()
         {
             InitializeComponent();
+            PutCities(comboBoxCities);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void PutCities(ComboBox box)
+        {
+            citis.Add("London", "london");
+            citis.Add("Istanbul", "istanbul");
+            citis.Add("Ankara", "ankara");
+            citis.Add("Brussels", "brussels");
+            citis.Add("Paris", "paris");
+            citis.Add("Samsun", "samsun");
+            citis.Add("Trabzon", "trabzon");
+
+            box.DataSource = new BindingSource(citis, null);
+            box.DisplayMember = "Key";
+            box.ValueMember = "Value";
         }
     }
 }
